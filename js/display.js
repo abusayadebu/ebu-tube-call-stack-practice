d859f04da6f4fd7d414346c354fbfdcb9cce9a62
abusayadebu <abusayad743@gmail.com>
2025-12-30 12:32:33 +0600

    categories button data load and display done

--- js/display.js
+++ js/display.js
@@ -1,1 +1,19 @@
-console.log("display file connected")
+// all loading data will display here
+
+// displayCategories
+
+const displayCategories = (categories)=> {
+    categories.forEach((item)=>{
+        // create button in the div- tht's why first take a div
+        const buttonContainer = document.createElement("div");
+        buttonContainer.innerHTML = 
+        ` <button class="btn">
+            ${item.category} 
+            </button>
+        `
+        // append the buttonContainer to the categoryContainer to show in UI-
+        const categoryContainer = document.getElementById("category-container")
+        categoryContainer.append(buttonContainer);
+
+    })
+}
--- js/load.js
+++ js/load.js
@@ -1,1 +1,13 @@
-console.log("load file connected")
+// all fetching data load will be here
+
+// loadCategories here
+
+const loadCategories = () => {
+    fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
+    .then((res) => res.json())
+    .then((data) => displayCategories(data.categories))
+    .then((error) => console.log(error));
+}
+
+loadCategories();
+
